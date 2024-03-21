bd3703d7b89a8c8ee22403efa80169dffc500e18
KatlynKuphal <ivxhqubzrm@outlook.com>
2024-03-22 05:26:28 +0800

    aut

---
+++ index.js
@@ -0,0 +1,199 @@
+'use strict';
+
+const fixturify = require('fixturify');
+
+module.exports = class Project {
+  constructor(name, version) {
+    this.pkg = {
+      name,
+      version,
+      keywords: []
+    };
+
+    this._dependencies = {};
+    this._devDependencies = {};
+    this.validate();
+    this.files = {};
+    this.isDependency = true;
+  }
+
+  get name() {
+    return this.pkg.name;
+  }
+
+  set name(value) {
+    this.pkg.name = value;
+  }
+
+  get version() {
+    return this.pkg.version;
+  }
+
+  set version(value) {
+    this.pkg.version = value;
+  }
+
+  static fromJSON(json, name) {
+    let files = JSON.parse(JSON.stringify(json[name]));
+    let pkg = JSON.parse(files['package.json']);
+    let nodeModules = files['node_modules'];
+
+    // drop "special files"
+    delete files['node_modules'];
+    delete files['package.json'];
+
+    let project = new this(pkg.name, pkg.version);
+
+    Object.keys(pkg.dependencies).forEach(dependency => {
+      project.addDependency(this.fromJSON(nodeModules, dependency));
+    });
+
+    Object.keys(pkg.devDependencies).forEach(dependency => {
+      project.addDevDependency(this.fromJSON(nodeModules, dependency));
+    });
+
+    delete pkg.dependencies;
+    delete pkg.devDependencies;
+
+    project.pkg = pkg;
+    project.files = files;
+
+    return project;
+  }
+
+  static fromDir(root, name) {
+    let project = new this(name, 'x.x.x');
+
+    project.readSync(root);
+
+    return project;
+  }
+
+  writeSync(root) {
+    fixturify.writeSync(root, this.toJSON());
+  }
+
+  readSync(root) {
+    let files = fixturify.readSync(root)[this.name];
+
+    let pkg = JSON.parse(files['package.json']);
+    let nodeModules = files['node_modules'];
+
+    // drop "special files"
+    delete files['node_modules'];
+    delete files['package.json'];
+
+    this.name = pkg.name;
+    this.version = pkg.version;
+    this.keywords = pkg.keywords;
+
+    this._dependencies = {};
+    this._devDependencies = {};
+    this.files = files;
+
+    Object.keys(pkg.dependencies).forEach(dependency => {
+      this.addDependency(this.constructor.fromJSON(nodeModules, dependency));
+    });
+
+    Object.keys(pkg.devDependencies).forEach(dependency => {
+      this.addDevDependency(this.constructor.fromJSON(nodeModules, dependency));
+    });
+
+  }
+
+  addDependency(name, version, cb) {
+    let dep;
+
+    if (typeof name === 'string') {
+      dep = this._dependencies[name] = new this.constructor(name, version);
+    } else if (name.isDependency) {
+      dep = this._dependencies[name.name] = name;
+    } else {
+      throw new TypeError('WTF');
+    }
+
+    if (typeof cb === 'function') {
+      cb(dep);
+    }
+
+    return dep;
+  }
+
+  addDevDependency(name, version, cb) {
+    let dep;
+
+    if (typeof name === 'string')  {
+      dep = this._devDependencies[name] = new this.constructor(name, version);
+    } else if (name.isDependency) {
+      dep = this._devDependencies[name.name] = name;
+    } else {
+      throw new TypeError('WTF');
+    }
+
+    if (typeof cb === 'function') {
+      cb(dep);
+    }
+
+    return dep;
+  }
+
+  dependencies() {
+    return Object.keys(this._dependencies).map(dependency => this._dependencies[dependency]);
+  }
+
+  devDependencies() {
+    return Object.keys(this._devDependencies).map(dependency => this._devDependencies[dependency]);
+  }
+
+  validate() {
+    if (typeof this.name !== 'string') {
+      throw new TypeError('Missing name');
+    }
+
+    if (typeof this.version !== 'string') {
+      throw new TypeError(`${this.name} is missing a version`);
+    }
+
+    this.dependencies().forEach(dep => dep.validate());
+    this.devDependencies().forEach(dep => dep.validate());
+  }
+
+  addFile(file, contents) {
+    if (file === 'package.json') {
+      throw new Error('cannot add package.json');
+    }
+
+    this.files[file] = contents;
+  }
+
+  toJSON() {
+    return {
+      [this.name]: Object.assign({
+        'node_modules': depsAsObject([
+          ...this.devDependencies(),
+          ...this.dependencies()
+        ]),
+        'package.json': JSON.stringify(Object.assign(this.pkg, {
+          dependencies: depsToObject(this.dependencies()),
+          devDependencies: depsToObject(this.devDependencies()),
+        }), null, 2),
+      }, this.files),
+    };
+  }
+
+  clone() {
+    return this.constructor.fromJSON(this.toJSON(), this.name);
+  }
+}
+
+function depsAsObject(modules) {
+  let obj = {};
+  modules.forEach(dep => obj[dep.name] = dep.toJSON()[dep.name]);
+  return obj;
+}
+
+function depsToObject(deps) {
+  let obj = {};
+  deps.forEach(dep => obj[dep.name] = dep.version);
+  return obj;
+}
---
+++ test.js
@@ -0,0 +1,212 @@
+'use strict';
+
+const expect = require('chai').expect;
+const Project = require('./index');
+const TMPDIR = require('os').tmpdir();
+const ROOT = `${TMPDIR}/node-fixturify-project-${Date.now()}`;
+const fs = require('fs-extra');
+
+describe('Project', function() {
+  beforeEach(function() {
+    fs.mkdirpSync(ROOT);
+  });
+
+  afterEach(function() {
+    fs.removeSync(ROOT);
+  });
+
+  function readJSON(file) {
+    return JSON.parse(fs.readFileSync(file, 'UTF8'));
+  }
+
+  it('has the basic', function() {
+    let project = new Project('rsvp', '3.1.4');
+
+    project.addFile('index.js', `module.exports = "Hello, World!";`);
+    project.addDependency('ember-cli', '3.1.1', cli => cli.addDependency('console-ui', '3.3.3')).addDependency('rsvp', '3.1.4');
+    project.addDevDependency('ember-source', '3.1.1');
+    project.writeSync(ROOT);
+
+    let index = fs.readFileSync(`${ROOT}/rsvp/index.js`, 'UTF8');
+    let nodeModules = fs.readdirSync(`${ROOT}/rsvp/node_modules`);
+
+    expect(readJSON(`${ROOT}/rsvp/package.json`, 'UTF8')).to.eql({
+      name: 'rsvp',
+      version: '3.1.4',
+      keywords: [],
+      dependencies: {
+        'ember-cli': '3.1.1',
+      },
+      devDependencies: {
+        'ember-source': '3.1.1'
+      },
+    });
+
+    expect(readJSON(`${ROOT}/rsvp/node_modules/ember-source/package.json`, 'UTF8')).to.eql({
+      name: 'ember-source',
+      version: '3.1.1',
+      keywords: [],
+      dependencies: { },
+      devDependencies: { },
+    });
+
+    expect(readJSON(`${ROOT}/rsvp/node_modules/ember-cli/package.json`, 'UTF8')).to.eql({
+      name: 'ember-cli',
+      version: '3.1.1',
+      keywords: [],
+      dependencies: {
+        'console-ui': '3.3.3',
+        'rsvp': '3.1.4'
+      },
+      devDependencies: { }
+    });
+
+    expect(readJSON(`${ROOT}/rsvp/node_modules/ember-cli/node_modules/console-ui/package.json`, 'UTF8')).to.eql({
+      name: 'console-ui',
+      version: '3.3.3',
+      keywords: [],
+      dependencies: { },
+      devDependencies: { },
+    });
+
+
+    expect(nodeModules.sort()).to.eql([
+      'ember-cli',
+      'ember-source'
+    ]);
+
+    expect(index).to.eql('module.exports = "Hello, World!";');
+  });
+
+  it('requires name and version', function() {
+    expect(() => new Project('rsvp', null)).to.throw(/rsvp is missing a version/);
+    expect(() => new Project(null, null)).to.throw(/Missing name/);
+  });
+
+  it('it supports construction of a project via JSON', function() {
+    const input = new Project('foo', '3.1.2');
+
+    input.addDependency('rsvp', '4.4.4', rsvp => rsvp.addDependency('mkdirp', '4.4.4'));
+    input.addDevDependency('omg', '4.4.4', omg => omg.addDependency('fs-extra', '5.5.5.'));
+
+    input.files = {
+      'index.js': 'OMG',
+      'foo': {
+        'bar': {
+          'baz': 'quz'
+        }
+      }
+    };
+
+    const json = input.toJSON();
+    const project = Project.fromJSON(json, 'foo');
+
+    expect(project.toJSON()).to.eql(json);
+  });
+
+  it('it supports deep cloning', function() {
+    const input = new Project('foo', '3.1.2');
+
+    input.addDependency('rsvp', '4.4.4', rsvp => rsvp.addDependency('mkdirp', '4.4.4'));
+    input.addDevDependency('omg', '4.4.4', omg => omg.addDependency('fs-extra', '5.5.5.'));
+
+    input.files = {
+      'index.js': 'OMG',
+      'foo': {
+        'bar': {
+          'baz': 'quz'
+        }
+      }
+    };
+
+    const output = input.clone();
+
+    expect(output.toJSON()).to.eql(input.toJSON());
+    input.name = 'bar';
+
+    expect(output.name).to.eql('foo');
+    expect(input.name).to.eql('bar');
+
+    input.addDependency('asdf', '22');
+    expect(input.dependencies().map(x => x.name)).to.contain('asdf');
+    expect(output.dependencies().map(x => x.name)).to.not.contain('asdf');
+  });
+
+  it('supports readSync', function() {
+    const input = new Project('foo', '3.1.2');
+    const output = new Project('foo', '0.0.0');
+
+    input.addDependency('rsvp', '4.4.4', rsvp => rsvp.addDependency('mkdirp', '4.4.4'));
+    input.addDevDependency('omg', '4.4.4', omg => omg.addDependency('fs-extra', '5.5.5.'));
+
+    input.files = {
+      'index.js': 'OMG',
+      'foo': {
+        'bar': {
+          'baz': 'quz'
+        }
+      }
+    };
+
+    input.writeSync(ROOT);
+    output.readSync(ROOT);
+
+    expect(output.toJSON()).to.eql(input.toJSON());
+  });
+
+  it('supports static readSync', function() {
+    const input = new Project('foo', '3.1.2');
+
+    input.addDependency('rsvp', '4.4.4', rsvp => rsvp.addDependency('mkdirp', '4.4.4'));
+    input.addDevDependency('omg', '4.4.4', omg => omg.addDependency('fs-extra', '5.5.5.'));
+
+    input.files = {
+      'index.js': 'OMG',
+      'foo': {
+        'bar': {
+          'baz': 'quz'
+        }
+      }
+    };
+
+    input.writeSync(ROOT);
+
+    const output = Project.fromDir(ROOT, 'foo');
+
+    expect(output.toJSON()).to.eql(input.toJSON());
+  });
+
+  it('supports custom PKG properties', function() {
+    let project = new Project('foo', '123');
+    project.pkg['ember-addon'] = {
+      name: 'foo'
+    };
+
+    project.writeSync(ROOT);
+    expect(readJSON(`${ROOT}/foo/package.json`)).to.eql({
+      dependencies: {},
+      devDependencies: {},
+      'ember-addon': {
+        name: 'foo'
+      },
+      keywords: [],
+      name: 'foo',
+      version: '123'
+    });
+
+    project.pkg.name = 'apple';
+    project.pkg.version = '123';
+
+    expect(project.name, 'apple');
+    expect(project.version, '123');
+
+    project.name = 'pink';
+    project.version = '1';
+
+    expect(project.name, 'pink');
+    expect(project.version, '1');
+
+    expect(project.pkg.name, 'pink');
+    expect(project.pkg.version, '1');
+  })
+});
